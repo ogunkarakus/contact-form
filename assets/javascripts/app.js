f6372274627258f680a5b974a9bb0b79a189ee76
Ogün Karakuş <kirk5bucuk@gmail.com>
2016-01-18 01:37:55 +0200

    Update app.js
    Signed-off-by: Ogün Karakuş <kirk5bucuk@gmail.com>

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,1 +1,28 @@
-/* app.js */
+/**
+ * Initialize the app.
+ *
+ * @param mixed callback
+ *
+ * @return void
+ */
+function app_init( callback ) {
+    if ( document.readyState != "loading" ) {
+        callback();
+    } else if ( document.addEventListener ) {
+        document.addEventListener( "DOMContentLoaded", callback );
+    } else {
+        document.attachEvent( "onreadystatechange", function () {
+            if ( document.readyState != "loading" ) {
+                callback();
+            }
+        } );
+    }
+}
+
+/* --- */
+
+app_init( function () {
+
+    //
+
+} );
