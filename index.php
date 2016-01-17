2b0b62d18d29611bf391977eb6aba8da425c2393
Ogün Karakuş <kirk5bucuk@gmail.com>
2016-01-18 01:21:21 +0200

    Update javascript and stylesheet dependencies
    Signed-off-by: Ogün Karakuş <kirk5bucuk@gmail.com>

--- index.php
+++ index.php
@@ -8,7 +8,21 @@
 ?><!DOCTYPE html>
 <html>
     <head>
+        <link href="http://cdnjs.cloudflare.com" rel="dns-prefetch" />
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" media="all" rel="stylesheet" type="text/css" />
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css" media="all" rel="stylesheet" type="text/css" />
         <meta charset="UTF-8" />
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/spin.min.js"></script>
+        <script>
+            /**/
+        </script>
+        <style>
+            /**/
+        </style>
     </head>
     <body>
         <!-- html > body -->
