1f028b876ef06e1ad565b2157530b30c3bad3998
Ogün Karakuş <kirk5bucuk@gmail.com>
2016-01-18 14:11:51 +0200

    Working on new contact form
    Signed-off-by: Ogün Karakuş <kirk5bucuk@gmail.com>

---
+++ libraries/Security.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Karakus\ContactForm;
+
+class Security
+{
+    //
+}
--- pages/e-mail/send.php
+++ pages/e-mail/send.php
@@ -1,3 +1,13 @@
 <?php
 
-    //
+    $is_ajax = isset( $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] );
+
+    if ( $is_ajax )
+        $is_ajax = $_SERVER[ 'HTTP_X_REQUESTED_WITH' ] == 'XMLHttpRequest';
+
+    if ( $is_ajax )
+    {
+        $name = $_POST[ 'name' ];
+        $e_mail_address = $_POST[ 'e-mail-address' ];
+        $message = $_POST[ 'message' ];
+    }
